f5b85044810ba8082e6183f7a2a35e8ab14730c7
andresccorrea <narvaezcamilo@hotmail.com>
2014-09-16 14:58:40 -0500

    Update ConsumerAnalysis.R

--- 03Analysis/ConsumerAnalysis.R
+++ 03Analysis/ConsumerAnalysis.R
@@ -17,27 +17,30 @@ if(!inherits(possibleError, "error")){
 #  complaints <- dbGetQuery(jdbcConnection, "select company_response as \"Response\" , 100*count(company_response)/(select count(company_response) from COMPLAINDATA where company='Bank of America')
 #AS \"Count\" from complaindata where company='Bank of America' group by company_response")
   responses <- dbGetQuery(jdbcConnection, "select * from (select company as \"company\", company_response as \"response\", 
-  (count(*) / (select count(*) from complaindata c2 where c1.company = c2.company group by company)) as \"percentage\"
-from complaindata c1
-where company in (select company from complaindata group by company having count(*) > 5000) 
-group by rollup(company, company_response) order by company) where \"response\" is not null")
+  (count(*) / (select count(*) from fin_complaint complt2, fin_company compny2 where compny2.company_id = complt2.company_id and
+    compny1.company = compny2.company group by company)) as \"percentage\" 
+                          from fin_complaint complt1, fin_company compny1 where complt1.company_id = compny1.company_id and
+                          complt1.company_id in (select company_id from fin_complaint group by 
+                          company_id having count(*) > 5000) group by rollup(company, company_response) 
+                          order by company
+  ) where \"response\" is not null;")
   
   products <- dbGetQuery(jdbcConnection, "select * from (select company as \"company\", product as \"product\", 
-  (count(*) / (select count(*) from complaindata c2 where 
-    c1.company = c2.company group by company)) as \"percentage\" 
-    from complaindata c1
-  where company in (select company from complaindata group by 
-    company having count(*) > 5000) group by rollup(company, product) 
+  (count(*) / (select count(*) from fin_complaint complt2, fin_company compny2 where compny2.company_id = complt2.company_id and
+    compny1.company = compny2.company group by company)) as \"percentage\" 
+    from fin_complaint complt1, fin_company compny1 where complt1.company_id = compny1.company_id and
+   complt1.company_id in (select company_id from fin_complaint group by 
+    company_id having count(*) > 5000) group by rollup(company, product) 
     order by company
-)  where \"product\" is not null")
+) where \"product\" is not null;")
   
   respByProduct <- dbGetQuery(jdbcConnection, "select * from (select product as \"product\", company_response as \"response\", 
-  (count(*) / (select count(*) from complaindata c2 where 
-    c1.product = c2.product group by product)) as \"percentage\" 
-    from complaindata c1
+  (count(*) / (select count(*) from fin_complaint complt2, fin_company compny2 where compny2.company_id = complt2.company_id and
+    complt1.product = complt2.product group by product)) as \"percentage\" 
+    from fin_complaint complt1, fin_company compny1 where compny1.company_id=complt1.company_id
     group by rollup(product, company_response) 
     order by product
-) where \"response\" is not null")
+) where \"response\" is not null;")
   
   dbDisconnect(jdbcConnection)
 }
@@ -54,9 +57,9 @@ head(complaints)
 #ggplot(data=complaints,  aes(x=factor(""),
 #                             y=Count, fill = Response) ) + geom_bar(stat="identity")
 
-ggplot(data=responses,  aes(x=company, y=percentage, fill = response) ) + geom_bar(stat="identity")
-ggplot(data=products,  aes(x=company, y=percentage, fill = product) ) + geom_bar(stat="identity")
-ggplot(data=respByProduct,  aes(x=product, y=percentage, fill = response) ) + geom_bar(stat="identity")
+ggplot(data=products,  aes(x=company, y=percentage, fill = product) ) + geom_bar(stat="identity") + ggtitle("Product Types for Companies With More Than 5000 Complaints") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggplot(data=responses,  aes(x=company, y=percentage, fill = response) ) + geom_bar(stat="identity") + ggtitle("Responses for Companies With More Than 5000 Complaints") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggplot(data=respByProduct,  aes(x=product, y=percentage, fill = response) ) + geom_bar(stat="identity") + ggtitle("Responses by Product Type") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 
 #ggplot(data = diamonds) + geom_histogram(aes(x = carat))
