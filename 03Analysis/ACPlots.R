641fa16593b6252a9f6b889432a0d1fa30f41352
unknown <narvaezcamilo@hotmail.com>
2014-09-16 15:05:12 -0500

    double plot 2012/2013

--- 03Analysis/ACPlots.R
+++ 03Analysis/ACPlots.R
@@ -22,19 +22,22 @@ head(nocomplains)
 
 ggplot(data = nocomplains, aes(x = COMP, y = COMPLAINS, fill = COMP)) + geom_bar(stat="identity")+
 scale_fill_manual(values= c("red1", "blue1", "darkorange", "green", "yellow", "cyan", "black", "purple", "orange", "darkred")) +
-ggtitle("Number of Complaints for the top ten companies") + scale_x_discrete(labels = "")
+ggtitle("Number of Complaints for the top ten companies")
 
 if(!inherits(possibleError, "error")){
-  datespec <- dbGetQuery(jdbcConnection, "Select EXTRACT(month from date_recieved)as \"Month\", 
-                                          count(complaint_id) as Complaints 
-                                          from fin_complaint  where Extract(year from date_recieved) = 2012
-                                          group by Extract(month from date_recieved)
-                                          order by Extract(month from date_recieved)")
+  datespec <- dbGetQuery(jdbcConnection, "Select * from (Select EXTRACT(month from date_recieved)as \"Month\", 
+count(complaint_id) as Complaints,
+Extract(year from date_recieved) as \"Year\"
+from fin_complaint  where Extract(year from date_recieved) = 2012 OR 
+Extract(year from date_recieved) = 2013
+group by rollup(Extract(year from date_recieved), Extract(month from date_recieved))) where \"Month\" is not NULL")
   
 }
 head(datespec)
 
-ggplot(data = datespec, aes(x = Month, y = COMPLAINTS)) + geom_line() +
-ggtitle("Number of Complaints in 2012")
+ggplot(data = datespec, aes(x = Month, y = COMPLAINTS, colour = factor(Year), group = Year )) + 
+geom_line() +
+ggtitle("Number of Complaints in 2012 and 2013") + scale_x_continuous(breaks=seq(1,12,1)) +
+scale_y_continuous(limits = c(3000,10000), breaks=seq(3000,10000,1000))
 
 dbDisconnect(jdbcConnection)
